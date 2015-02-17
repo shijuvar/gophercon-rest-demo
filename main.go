b3a4fc24914b183b6390fcdfde546ab59de7dcf4
Shiju Varghese <shiju.varghese@outlook.com>
2015-02-17 12:33:59 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
+	"gopkg.in/mgo.v2"
+        "gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
 	"time"
