06a9279ea47a4bd5cd25c42c673662894764005c
Shiju Varghese <shiju.varghese@outlook.com>
2015-02-18 17:06:08 +0530

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
-// gophercon15-demo project doc.go
-
 /*
-gophercon15-demo document
+REST API demo written for GopherCon India 2015.
+Source code for the session "Building RESTful Services With Go and MongoDB"
+Third-part packages used: "github.com/gorilla/mux", "gopkg.in/mgo.v2"
 */
 package main
